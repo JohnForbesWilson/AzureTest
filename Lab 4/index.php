d3079198191e6902b433ac2fbc5dfec87913fb19
John <1317066@rgu.ac.uk>
2016-02-24 16:20:58 +0000

    Lab 4 start...   oy...

--- Lab 4/index.php
+++ Lab 4/index.php
@@ -5,13 +5,13 @@ Author: John F. Wilson
 ID: 	1317066
 Module: CMM503 Web Systems Development
 Date:	24 February 2016
-Title: 	Lab 4 - Section 2
+Title: 	Lab 4 - Section 2, Section 3
 -->
 
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Lab 4 Section 2</title>
+    <title>Lab 4 Section 3</title>
     <link rel="stylesheet" type="text/css" href="assets/css/style.css"/>
 </head>
 <body>
@@ -26,6 +26,17 @@ Title: 	Lab 4 - Section 2
  * Time: 15:57
  */
 echo "Hello World";
+echo "Hello, Beautiful World";
+echo "Hello"." "."My Lovely"."!";
+echo 5*7;
+$myname = "Frodo Baggins";
+$myage = 111;
+echo "Hello, My Name is ".$myname." and My Age is ".$myage;
+echo "I get printed";
+// I don't get printed because I am a comment.
+/* and, furthermore, i don't get printed either
+because I am also a comment.*/
+
 ?>
 
     </body>
