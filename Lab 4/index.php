7f1d7d5f2fad47e2e46237dd1ef52c54d7c8d4f3
John <1317066@rgu.ac.uk>
2016-02-24 16:06:17 +0000

    Lab 4 start...   oy...

---
+++ Lab 4/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<!--
+Author: John F. Wilson
+ID: 	1317066
+Module: CMM503 Web Systems Development
+Date:	24 February 2016
+Title: 	Lab 4 - Section 2
+-->
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Lab 4 Section 2</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css"/>
+</head>
+<body>
+<p>
+
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: John
+ * Date: 24/02/2016
+ * Time: 15:57
+ */
+echo "Hello World";
+?>
+
+    </body>
+</html>
